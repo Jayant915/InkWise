6f668b4434080bc8ee4e1e93d604368d6f3581c6
Jayant Kishore <jayantkishore9155@gmail.com>
2025-04-15 21:12:19 +0530

    upload feature added

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,11 @@
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+
+from .forms import UploadImageForm
+from django.core.files.storage import default_storage
+from PIL import Image
+import pytesseract
 
 #def home(request):
  #   return HttpResponse("Hello, Django!")
@@ -26,3 +31,25 @@ def contact(request):
 
 def services(request):
     return render(request, 'services.html')
+
+
+def upload_image(request):
+    if request.method == 'POST':
+        form = UploadImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.cleaned_data['image']
+            # Save the image
+            image_name = default_storage.save('uploaded_images/' + image.name, image)
+            image_path = default_storage.path(image_name)
+
+            # Process the image
+            try:
+                pil_image = Image.open(image_path)
+                text = pytesseract.image_to_string(pil_image)
+            except Exception as e:
+                return HttpResponse(f"Error processing image: {e}")
+
+            return HttpResponse(f"Image uploaded and processed. Recognized text: {text}")
+    else:
+        form = UploadImageForm()
+    return render(request, 'upload.html', {'form': form})
